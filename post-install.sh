68a80d0fe99fb959cdfa54de59d01f74b44719cf
Tanwei Sun <tanwei.sun@gmail.com>
2013-11-04 21:41:27 +0800

    change folder name in dropbox

--- post-install.sh
+++ post-install.sh
@@ -1,7 +1,7 @@
 # Configure goagent
-#cd ~/code/goagent/local
-#rm ./proxy.ini
-#ln -s ~/Dropbox/bootstrap/goagent/proxy.ini ./proxy.ini
+cd ~/code/goagent/local
+rm ./proxy.ini
+ln -s ~/Dropbox/appdata/goagent/proxy.ini ./proxy.ini
 
 
 # Synchronize Sublime Text 2 Settings
@@ -9,6 +9,6 @@ cd ~/Library/Application\ Support/Sublime\ Text\ 2
 rm -r Installed\ Packages
 rm -r Packages
 rm -r Pristine\ Packages
-ln -s ~/Dropbox/bootstrap/sublime/Installed\ Packages ./Installed\ Packages
-ln -s ~/Dropbox/bootstrap/sublime/Packages ./Packages
-ln -s ~/Dropbox/bootstrap/sublime/Pristine\ Packages ./Pristine\ Packages
+ln -s ~/Dropbox/appdata/sublime/Installed\ Packages ./Installed\ Packages
+ln -s ~/Dropbox/appdata/sublime/Packages ./Packages
+ln -s ~/Dropbox/appdata/sublime/Pristine\ Packages ./Pristine\ Packages
