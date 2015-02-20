fd6d44b2d1c8c55305bf18baa9f859e3b1d47c60
Tanwei Sun <tanwei.sun@gmail.com>
2015-02-20 12:09:54 +0800

    change application configuration location to cloud station

--- post-install.sh
+++ post-install.sh
@@ -1,7 +1,7 @@
 # Configure goagent
 cd ~/code/goagent/local
 rm ./proxy.ini
-ln -s ~/Dropbox/appdata/goagent/proxy.ini ./proxy.ini
+ln -s ~/app/goagent/proxy.ini ./proxy.ini
 
 
 # Synchronize Sublime Text 2 Settings
@@ -9,16 +9,16 @@ cd ~/Library/Application\ Support/Sublime\ Text\ 2
 rm -r Installed\ Packages
 rm -r Packages
 rm -r Pristine\ Packages
-ln -s ~/Dropbox/appdata/sublime/Installed\ Packages ./Installed\ Packages
-ln -s ~/Dropbox/appdata/sublime/Packages ./Packages
-ln -s ~/Dropbox/appdata/sublime/Pristine\ Packages ./Pristine\ Packages
+ln -s ~/app/sublime/Installed\ Packages ./Installed\ Packages
+ln -s ~/app/sublime/Packages ./Packages
+ln -s ~/app/sublime/Pristine\ Packages ./Pristine\ Packages
 
 # Synchronize Photoshop Presets
 cd /Applications/Adobe\ Photoshop\ CS6
 rm -r Presets
-ln -s ~/Dropbox/appdata/photoshop/Presets ./Presets
+ln -s ~/app/photoshop/Presets ./Presets
 
 # Synchronize Apache Configuration
-cp ~/Dropbox/appdata/apache/tanweisun.conf /etc/apache2/users
+sudo cp ~/app/apache/tanweisun.conf /etc/apache2/users
 
 cd ~
